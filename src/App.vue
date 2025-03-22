d855ada7d893d55a326f1b708c0744c8903cc190
Felipe Faria <felipe.faria@azurebrasil.cloud>
2025-03-22 15:09:29 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  data() {
+    return {
+      numero: 0,
+      nome: "Options Vue"
+    }
+  },
+  methods: {
+    adicionarNumero() {
+      this.numero++
+    },
+    removerNumero() {
+      this.numero--
+    }
+  }
+}
+</script>
+
+<template>
+  <h2>{{ nome }}</h2>
+  <p>
+    {{ numero }}
+  </p>
+  <button @click="adicionarNumero">+</button>
+  <button @click="removerNumero">-</button>
+  
+</template>
+
+<style scoped>
+
+</style> 
